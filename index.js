c676d583eb92b1ab0df7895df1520485660238e4
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2024-10-06 11:00:30 +0300

    add a "bit more" blocks (also license moment)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Rix-Ex. A revamp of RixxyX.
+// Available under GNU AGPLv3.
 
 (function() {
 	var cols = []
@@ -19,7 +20,16 @@
 			this.name = "Rix-Ex"
 			this.id = "RixEx"
 			this.cols = ["#00ff33", "#00801a", "#00801a"]
-			this.menus = {}
+			this.menus = {
+				rgb: {
+					acceptReporters: true,
+					items: "RGB".split("")
+				},
+				tf: {
+					acceptReporters: false,
+					items: [true, false]
+				}
+			}
 		}
 		getInfo() {
 			return {
@@ -30,7 +40,7 @@
 				blocks: [
 					{
 						opcode: "newColor",
-						text: "color from [col]",
+						text: "color ID from [col]",
 						blockType: Scratch.BlockType.REPORTER,
 						arguments: {
 							col: {
@@ -38,12 +48,94 @@
 								defaultValue: this.cols[0]
 							}
 						}
+					},
+					{
+						opcode: "colorEx",
+						text: "color ID [col] exists?",
+						blockType: Scratch.BlockType.BOOLEAN,
+						arguments: {
+							col: {
+								type: Scratch.ArgumentType.NUMBER
+							}
+						}
+					},
+					{
+						opcode: "rgbOfCol",
+						text: "[rgb] of [col]",
+						blockType: Scratch.BlockType.REPORTER,
+						arguments: {
+							rgb: {
+								type: Scratch.ArgumentType.STRING,
+								menu: "rgb",
+								defaultValue: "R"
+							},
+							col: {
+								type: Scratch.ArgumentType.NUMBER
+							}
+						}
+					},
+					{
+						opcode: "setRgbInColTo",
+						text: "set [col]'s [rgb] to [v]",
+						blockType: Scratch.BlockType.COMMAND,
+						arguments: {
+							col: {
+								type: Scratch.ArgumentType.NUMBER
+							},
+							rgb: {
+								type: Scratch.ArgumentType.STRING,
+								menu: "rgb",
+								defaultValue: "R"
+							},
+							v: {
+								type: Scratch.ArgumentType.NUMBER,
+								defaultValue: parseInt(cols[0].slice(1, 3), 16)
+							}
+						}
+					},
+					"---",
+					{
+						opcode: "bool",
+						text: "[v]"
+						arguments: {
+							v: {
+								type: Scratch.ArgumentType.STRING,
+								menu: "tf",
+								defaultValue: true
+							}
+						}
+					},
+					{
+						opcode: "bool",
+						text: "[v]"
+						arguments: {
+							v: {
+								type: Scratch.ArgumentType.STRING,
+								menu: "tf",
+								defaultValue: false
+							}
+						}
 					}
 				]
 			}
 		}
 		newColor(args, util) {
-			return new color(0, 0, 0)
+			return new color(0, 0, 0).id
+		}
+		colorEx(args, util) {
+			return Object.prototype.toString.call(cols[Scratch.Cast.toNumber(args.rgb)] ?? false) != Object.prototype.toString.call(false)
+		}
+		rgbOfCol(args, util) {
+			return (cols[Scratch.Cast.toNumber(args.rgb)] ?? Reflect.construct(color, Array(3).fill(0)))[Scratch.Cast.toString(args.rgb).toLowerCase()]
+		}
+		setRgbInColTo(args, util) {
+			var c = cols[Scratch.Cast.toNumber(args.rgb)] ?? Reflect.construct(color, Array(3).fill(0))
+			var k = Scratch.Cast.toString(args.rgb).toLowerCase()
+			var v = Scratch.Cast.toNumber(args.v)
+			c[k] = v
+		},
+		bool(args) {
+			return JSON.parse(args.v)
 		}
 	}
 	Scratch.extensions.register(new RixEx())
